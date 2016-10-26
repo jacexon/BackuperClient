c474c3260303006d787985dada44720fc2fc2179
jacexon <polakjacek@gmail.com>
2016-10-26 20:45:33 +0200

    Hej

--- src/sample/clientScreenController.java
+++ src/sample/clientScreenController.java
@@ -8,21 +8,23 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import javax.annotation.Resource;
+import javax.swing.*;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import static javafx.collections.FXCollections.*;
-
 public class clientScreenController implements Initializable{
     @FXML
     private TableView<ServerTable> table;
@@ -46,13 +48,31 @@ public class clientScreenController implements Initializable{
     private TableColumn<ServerTable,String> versionColumn;
 
     @FXML private MenuItem upload_menuitem;
+    @FXML private MenuItem download_menuitem;
+
+    @FXML private Menu file_menu;
+    @FXML private Menu about_menu;
+    @FXML private Menu info_menu;
 
     public void handleUpload(ActionEvent event) throws IOException{
-        Parent selectingFilesScreen= FXMLLoader.load(getClass().getResource("selectingFilesScreen.fxml"));
+        //Parent selectingFilesScreen= FXMLLoader.load(getClass().getResource("selectingFilesScreen.fxml"));
         Stage selectingFilesStage = new Stage();
-        selectingFilesStage.setScene(new Scene(selectingFilesScreen,400,400));
-        selectingFilesStage.setTitle("FileChooser");
-        selectingFilesStage.show();
+        //selectingFilesStage.setScene(new Scene(selectingFilesScreen,400,400));
+        //selectingFilesStage.setTitle("FileChooser");
+        //selectingFilesStage.show();
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose file(s)...");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home"))
+        );
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("All Images", "*.*"),
+                new FileChooser.ExtensionFilter("JPG", "*.jpg"),
+                new FileChooser.ExtensionFilter("GIF", "*.gif"),
+                new FileChooser.ExtensionFilter("BMP", "*.bmp"),
+                new FileChooser.ExtensionFilter("PNG", "*.png")
+        );
+        fileChooser.showOpenDialog(selectingFilesStage);
     }
 
 
@@ -73,6 +93,10 @@ public class clientScreenController implements Initializable{
 
     @Override
     public void initialize(URL location, ResourceBundle resources){
+
+        upload_menuitem.setAccelerator(new KeyCodeCombination(KeyCode.U, KeyCombination.CONTROL_DOWN));
+        download_menuitem.setAccelerator(new KeyCodeCombination(KeyCode.D, KeyCombination.CONTROL_DOWN));
+
         idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
         filenameColumn.setCellValueFactory(new PropertyValueFactory<>("FileName"));
         extensionColumn.setCellValueFactory(new PropertyValueFactory<>("Extension"));
@@ -82,6 +106,8 @@ public class clientScreenController implements Initializable{
 
         table.setItems(data);
     }
+
+
 
 
 
--- src/sample/selectingFilesScreenController.java
+++ src/sample/selectingFilesScreenController.java
@@ -4,14 +4,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
 import javafx.scene.control.*;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javax.swing.*;
 import javafx.event.*;
 import java.io.IOException;
 
 public class selectingFilesScreenController {
-
-
 
 
 }
