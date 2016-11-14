aca355cc41c7ae2d2e42a87871219f936ee42df6
jacexon <polakjacek@gmail.com>
2016-11-14 15:06:23 +0100

    Hej

--- src/sample/clientScreenController.java
+++ src/sample/clientScreenController.java
@@ -18,12 +18,13 @@ import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-public class clientScreenController implements Initializable{
+public class clientScreenController implements Initializable {
     @FXML
     private TableView<ServerTable> table;
 
@@ -31,29 +32,34 @@ public class clientScreenController implements Initializable{
     private TableColumn<ServerTable, Integer> idColumn;
 
     @FXML
-    private TableColumn<ServerTable,String> filenameColumn;
+    private TableColumn<ServerTable, String> filenameColumn;
 
     @FXML
-    private TableColumn<ServerTable,String> extensionColumn;
+    private TableColumn<ServerTable, String> extensionColumn;
 
     @FXML
-    private TableColumn<ServerTable,Long> sizeColumn;
+    private TableColumn<ServerTable, Long> sizeColumn;
 
     @FXML
-    private TableColumn<ServerTable,String> lastModifiedColumn;
+    private TableColumn<ServerTable, String> lastModifiedColumn;
 
     @FXML
-    private TableColumn<ServerTable,String> versionColumn;
+    private TableColumn<ServerTable, String> versionColumn;
 
-    @FXML private MenuItem upload_menuitem;
-    @FXML private MenuItem download_menuitem;
+    @FXML
+    private MenuItem upload_menuitem;
+    @FXML
+    private MenuItem download_menuitem;
 
-    @FXML private Menu file_menu;
-    @FXML private Menu about_menu;
-    @FXML private Menu info_menu;
+    @FXML
+    private Menu file_menu;
+    @FXML
+    private Menu about_menu;
+    @FXML
+    private Menu info_menu;
     private List<File> chosenFiles;
 
-    public void handleUpload(ActionEvent event) throws IOException{
+    public void handleUpload(ActionEvent event) throws Exception {
         //Parent selectingFilesScreen= FXMLLoader.load(getClass().getResource("selectingFilesScreen.fxml"));
         Stage selectingFilesStage = new Stage();
         //selectingFilesStage.setScene(new Scene(selectingFilesScreen,400,400));
@@ -76,45 +82,52 @@ public class clientScreenController implements Initializable{
         chosenFiles = fileChooser.showOpenMultipleDialog(selectingFilesStage);
         System.out.println(chosenFiles);
 
+        try {
+            if (chosenFiles != null) {
+                for (File f : chosenFiles) {
+                    System.out.println(f.getName() + " " + f.lastModified());
+                    if (!(BackupClient.getServer().checkFileOnServer(f.getName(), f.lastModified()))) {
+                        BackupClient.send(BackupClient.getServer(), f.getPath(), f.getName(), BackupClient.getFileExtension(f), f.lastModified());
+                        System.out.println("Przesłano plik: " + f.getName() + " " + "!");
+                    }
 
-        if (chosenFiles!=null){
-            for (File f : chosenFiles){
-                if (!BackupClient.getServer().checkFileOnServer(f.getName(),f.lastModified())){
-                    BackupClient.send(BackupClient.getServer(),f.getPath(),f.getName(),BackupClient.getFileExtension(f), f.lastModified());
-                    System.out.println("Przesłano plik: " + f.getName() + " " + "!");
                 }
-
             }
+        } catch (Exception e) {
+            System.out.println(e.getCause().getMessage());
         }
-
     }
 
 
-    public File getChosenFile(int index){
+    public File getChosenFile(int index) {
         return chosenFiles.get(index);
     }
-    public File getChosenFile(){
+
+    public File getChosenFile() {
         return chosenFiles.remove(0);
     }
-    public int getNumberOfChosenFiles(){return chosenFiles.size();}
+
+    public int getNumberOfChosenFiles() {
+        return chosenFiles.size();
+    }
 
     private int iNumber = 1;
 
     private final ObservableList<ServerTable> data = FXCollections.observableArrayList(
-            new ServerTable(iNumber++,"lol",".jpg","2015-12-12",1212122,"2.0"),
-            new ServerTable(iNumber++,"be",".png","2015-12-12", 20102121, "2.1"),
-            new ServerTable(iNumber++,"l",".jpg","2015-12-12",1212122,"2.0"),
-            new ServerTable(iNumber++,"b.",".png","2015-12-12", 20102121, "2.1")
+            new ServerTable(iNumber++, "lol", ".jpg", "2015-12-12", 1212122, "2.0"),
+            new ServerTable(iNumber++, "be", ".png", "2015-12-12", 20102121, "2.1"),
+            new ServerTable(iNumber++, "l", ".jpg", "2015-12-12", 1212122, "2.0"),
+            new ServerTable(iNumber++, "b.", ".png", "2015-12-12", 20102121, "2.1")
     );
 
 
-    public void addRow(int id, String filename, String extension, String lastModified, long size, String version){
-        data.add(new ServerTable(iNumber++,filename,extension,lastModified,size,version));
+    public void addRow(int id, String filename, String extension, String lastModified, long size, String version) {
+        data.add(new ServerTable(iNumber++, filename, extension, lastModified, size, version));
         table.setItems(data);
     }
 
     @Override
-    public void initialize(URL location, ResourceBundle resources){
+    public void initialize(URL location, ResourceBundle resources) {
 
         upload_menuitem.setAccelerator(new KeyCodeCombination(KeyCode.U, KeyCombination.CONTROL_DOWN));
         download_menuitem.setAccelerator(new KeyCodeCombination(KeyCode.D, KeyCombination.CONTROL_DOWN));
@@ -129,9 +142,6 @@ public class clientScreenController implements Initializable{
         table.setItems(data);
     }
 
-
-
-
-
-
 }
+
+
